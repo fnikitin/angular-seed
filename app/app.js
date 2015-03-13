ecb7d8cb8cbdc6e13681a1d074800542f0ab412a
fnikitin <frederic.nikitin@gmail.com>
2015-03-13 18:29:48 +0100

    add a new module for tri-state checkbox demo

--- app/app.js
+++ app/app.js
@@ -2,13 +2,14 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-    'myApp.view3',
-    'myApp.cart',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        'ngRoute',
+        'myApp.view1',
+        'myApp.view2',
+        'myApp.view3',
+        'myApp.cart',
+        'myApp.checkboxes',
+        'myApp.version'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
---
+++ app/checkboxes/checkboxes.js
@@ -0,0 +1,97 @@
+// http://jsfiddle.net/cjwprostar/M4vGj/6/
+'use strict';
+
+angular.module('myApp.checkboxes', ['ngRoute'])
+
+// inject object $routeProvider that probably comes from module 'ngRoute'
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/checkboxes', {
+        templateUrl: 'checkboxes/checkboxes.html',
+        controller: 'MainCtrl'
+    });
+}])
+
+.controller('MainCtrl', ['$scope', function($scope) {
+    $scope.model = {
+        allEaten: false,
+        people: [
+            {
+                name: "Bob",
+                fruits: [
+                    { type: 'Apple', eaten: false },
+                    { type: 'Banana', eaten: false },
+                    { type: 'Pear', eaten: true },
+                    { type: 'Tomato', eaten: false },
+                    { type: 'Grapefruit', eaten: true },
+                ]
+            },
+            {
+                name: "Joe",
+                fruits: [
+                    { type: 'Apple', eaten: true },
+                    { type: 'Banana', eaten: true },
+                    { type: 'Pear', eaten: true },
+                    { type: 'Tomato', eaten: true },
+                    { type: 'Grapefruit', eaten: true },
+                ]
+            }
+        ]
+    };
+}])
+
+/**
+ * Directive for an indeterminate (tri-state) checkbox.
+ * Based on the examples at http://stackoverflow.com/questions/12648466/how-can-i-get-angular-js-checkboxes-with-select-unselect-all-functionality-and-i
+ */
+.directive('indeterminateCheckbox', [function() {
+    return {
+        scope: true,
+        require: '?ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+            var childList = attrs.childList;
+            var property = attrs.property;
+
+            // Bind the onChange event to update children
+            element.bind('change', function() {
+                scope.$apply(function () {
+                    var isChecked = element.prop('checked');
+
+                    // Set each child's selected property to the checkbox's checked property
+                    angular.forEach(scope.$eval(childList), function(child) {
+                        child[property] = isChecked;
+                    });
+                });
+            });
+
+            // Watch the children for changes
+            scope.$watch(childList, function(newValue) {
+                var hasChecked = false;
+                var hasUnchecked = false;
+
+                // Loop through the children
+                angular.forEach(newValue, function(child) {
+                    if (child[property]) {
+                        hasChecked = true;
+                    } else {
+                        hasUnchecked = true;
+                    }
+                });
+
+                // Determine which state to put the checkbox in
+                if (hasChecked && hasUnchecked) {
+                    element.prop('checked', false);
+                    element.prop('indeterminate', true);
+                    if (modelCtrl) {
+                        modelCtrl.$setViewValue(false);
+                    }
+                } else {
+                    element.prop('checked', hasChecked);
+                    element.prop('indeterminate', false);
+                    if (modelCtrl) {
+                        modelCtrl.$setViewValue(hasChecked);
+                    }
+                }
+            }, true);
+        }
+    };
+}]);
